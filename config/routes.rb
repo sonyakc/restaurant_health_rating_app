a1b854f16cf0ce2e43617ef9961bcfa0c351adb9
Sonya <sonyakc.2007@gmail.com>
2014-05-31 16:25:30 -0400

    Add a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   root 'restaurants#index'
 
   get 'restaurants/search_by_rating/', to: 'restaurants#search_by_rating', as: :rating_search
